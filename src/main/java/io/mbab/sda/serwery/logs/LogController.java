8b477d654cbd1759e9131bdbf0190b650d8f3c15
Michał Babula <michal.babula@britenet.com.pl>
2019-06-15 11:46:37 +0200

    initial commit

---
+++ src/main/java/io/mbab/sda/serwery/logs/LogController.java
@@ -0,0 +1,26 @@
+package io.mbab.sda.serwery.logs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/logs")
+@Slf4j
+public class LogController {
+
+    @GetMapping
+    String menu() {
+        return "<button onClick=\"location.href='logs/generate'\"><b>GENERATE_LOGS<b></button>";
+    }
+
+    @GetMapping("/generate")
+    void generate() {
+        log.info("Generated INFO log");
+        log.warn("Generated WARN log");
+        log.error("Generated ERROR log");
+        log.debug("Generated DEBUG log");
+
+    }
+}
