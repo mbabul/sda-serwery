611698a405ce54e403df356b50fb2457334c7a7f
Michał Babula <michal.babula@britenet.com.pl>
2020-02-22 13:33:56 +0100

    Feature - Add methods to get and set logging level in mbean

--- src/main/java/io/mbab/sda/serwery/JmxService.java
+++ src/main/java/io/mbab/sda/serwery/JmxService.java
@@ -1,6 +1,9 @@
 package io.mbab.sda.serwery;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Component;
@@ -10,8 +13,20 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class JmxService {
 
+    private LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+
     @ManagedOperation
-    public void managedOperation(String param) {
-        log.info("Invoked operation via jmx with parameter: {}", param);
+    public void setLoggingLevel(String level) {
+        loggerContext.getLogger("io.mbab.sda.serwery")
+                .setLevel(Level.valueOf(level));
+
+        log.info("Logging level set to: {}", level);
+    }
+
+    @ManagedOperation
+    public String getLoggingLevel() {
+        return loggerContext.getLogger("io.mbab.sda.serwery")
+                .getLevel()
+                .toString();
     }
 }
